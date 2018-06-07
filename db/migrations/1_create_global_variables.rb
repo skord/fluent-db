b9a20e557070c85582345c6c218d84f9273bf137
Mike Danko <mike@l4m3.com>
2018-06-07 13:24:13 -0400

    tada

--- db/migrations/1_create_global_variables.rb
+++ db/migrations/1_create_global_variables.rb
@@ -4,6 +4,9 @@ Sequel.migration do
       String :hostname, null: :false
       String :variable_name, null: false
       String :value
+      DateTime :created_at
+      DateTime :updated_at
+      primary_key [:hostname, :variable_name]
     end
   end
   down do
